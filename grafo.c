b2e1086a41c633a80d219831b16346e2bc0408a0
mspavanelli <mspavanelli@gmail.com>
2015-06-16 02:31:26 -0300

    Improvments

--- grafo.c
+++ grafo.c
@@ -3,7 +3,21 @@
 
 int main()
 {
-	printf("OK\n");
+	imprime_grafo();
+	insere_aresta(1,2);
+	imprime_grafo();
 
 	return 0;
+}
+
+void profundidade( int i )
+{
+	int adj;
+	flag[i] = 1;	// descoberto
+	for ( adj = 1; adj <= V; adj++ )
+	{
+		if ( grafo[i][adj] == 1 && flag[adj] == 0 )
+			profundidade( adj );
+	}
+	flag[i] = 2;	// finalizado (pode imprimir)
 }
--- grafo.h
+++ grafo.h
@@ -2,4 +2,48 @@
 #define TAMANHO 5
 
 /* Grafo em matriz de adjacências */
-bool grafo[TAMANHO][TAMANHO];
+bool grafo[TAMANHO+1][TAMANHO+1];
+
+void imprime_grafo()
+{
+	int i, j;
+
+	printf("   ");
+	for ( i = 1; i <= TAMANHO; i++ ) printf("%d ", i);
+	printf("\n");
+	for ( i = 1; i <= TAMANHO; i++ )
+	{
+		printf("%d: ", i);
+		for ( j = 1; j <= TAMANHO; j++ )
+		{
+			printf( grafo[i][j] ? "1 " : "0 " );
+		}
+		printf("\n");
+	}
+}
+
+bool insere_aresta( int origem, int destino )
+{
+	if ( grafo[origem][destino] ) return false;
+	grafo[origem][destino] = true;
+	return true;
+}
+
+bool remove_aresta( int origem, int destino )
+{
+	if ( !grafo[origem][destino] ) return false;
+	grafo[origem][destino] = false;
+	return true;
+}
+
+int grau( int vertice )
+{
+	int g = 0, i;
+	for ( i = 0; i <= TAMANHO; i++ )
+		if ( grafo[vertice][i] ) g++;
+
+	return g;
+}
+
+void busca_profundidade( int vertice );
+void busca_largura( int vertice );
